9a7e1744ab09233be7dbeff9dd6990500e95693d
CH144 <177772377+CH144@users.noreply.github.com>
2026-03-18 13:19:06 +0000

    Add dollar sign to salary when displayed

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -56,7 +56,7 @@ public class PersonCard extends UiPart<Region> {
         phone.setText(person.getPhone().value);
         address.setText(person.getAddress().value);
         email.setText(person.getEmail().value);
-        salary.setText(person.getSalary().value);
+        salary.setText("$" + person.getSalary().value);
         person.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
